b543527e3cba3b42701852e41c7eb8cffed55cc3
python-nerd-3 <117922845+python-nerd-3@users.noreply.github.com>
2022-11-23 19:50:34 -0500

    update
    yungus

---
+++ js-testing/main.js
@@ -0,0 +1,15 @@
+let x = "h"
+console.log(x)
+//let behaves like normally declaring variables in python
+var y = "finger named kid"
+function funkytion() {
+    console.log(y)
+}
+funkytion()
+
+function funkiertion() {
+    var z = "hamborgir"
+}
+
+funkiertion()
+console.log(z)
